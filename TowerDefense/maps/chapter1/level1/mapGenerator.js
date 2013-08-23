2bb8e32ffccd255f3ebcfe5059708b5243049a35
Ivan Merola <merola.ivan@gmail.com>
2013-08-23 11:10:03 -0300

    Refactoring level implementation

---
+++ TowerDefense/maps/chapter1/level1/mapGenerator.js
@@ -0,0 +1,40 @@
+maps = [];
+
+function createMap(map, npcs, towers, id, npc, tower, bits, bit0, life, waveQty, actualWave){
+
+	var map = map;
+	map.npcs = npcs;
+	map.towers = towers;
+	map.id = id;
+	map.npc = npc;
+	map.tower = tower;
+	map.bits = bits;
+	map.bits[0] = bit0;
+	map.life = life;
+	map.waveQty = waveQty;
+	map.actualWave = actualWave;
+	
+	return map;
+
+}
+
+function initAttributes(name, npcSprite, npcWidth, npcHeight, towerSprite, towerWidth,
+towerHeight, towerPlaceWidth, towerPlaceHeight, towerQtyFrames, range, bullet){
+
+	var map = {};
+	map.name = name;
+	map.npcSprite = npcSprite;
+	map.npcWidth = npcWidth;
+	map.npcHeight = npcHeight;
+	map.towerSprite = towerSprite;
+	map.towerWidth = towerWidth;
+	map.towerHeight = towerHeight;
+	map.towerPlaceWidth = towerPlaceWidth;
+	map.towerPlaceHeight = towerPlaceHeight;
+	map.towerQtyFrames = towerQtyFrames;
+	map.range = range;
+	map.bullet = bullet;
+	maps.push(map);
+	return map;
+
+}
